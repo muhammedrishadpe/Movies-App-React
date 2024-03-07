7266cf727a65bb96566fa2c0e6cb38a750df6af3
muhammedrishadpe <rishadpe@gmail.com>
2024-03-07 10:28:27 +0100

    updated to axios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,20 +3,26 @@ import { Logout } from "../Logout/Logout";
 import { MovieHover } from "../../components/MovieHover";
 import {useDarkMode} from '../../context/DarkModeContext'
 import "../Home/Home.css";
+import axios from "axios";
+
+const API_URL = "https://api.themoviedb.org/3/discover/movie?api_key=a1c453f07dc75f98d9fb4a3c4ee5abfc&language=en-US&page=1&include_adult=false";
+
 export const Home = () => {
   const [moviesList, setMoviesList] = useState([]);
+  const [error,setError] = useState(null);
   const [hoveredIndex, setHoveredIndex] = useState(null);
-
-
   const handleHover = (index) => {
     setHoveredIndex(index);
-  };
+  };  
   const getMovies = () => {
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=a1c453f07dc75f98d9fb4a3c4ee5abfc&language=en-US&page=1&include_adult=false"
-    )
-      .then((res) => res.json())
-      .then((json) => setMoviesList(json.results));
+    axios
+    .get(API_URL)
+    .then((response) => {
+      setMoviesList(response.data.results);
+    })
+    .catch((error) => {
+      setError(error);
+    });
   };
   useEffect(() => {
     getMovies();
